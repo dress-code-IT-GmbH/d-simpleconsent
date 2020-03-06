a96976d47d9197a261adb9c67877b06be4977745
war <thomas@warwaris.at>
2020-03-06 13:56:17 +0100

    migrate scripts

---
+++ install/opt/bin/migrate.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
+
+source /etc/profile.d/py_venv.sh
+export PYTHONPATH=$APPHOME:$APPHOME/simpleconsent
+python manage.py migrate
